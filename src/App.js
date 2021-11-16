7088b69e6237c38dc23500447357173abfd58e1c
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-11-16 19:18:50 +0530

    header styling part 1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-
-<Header/>
+      <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,46 +1,54 @@
- import React from 'react'
- import './Header.css';
- 
- function Header() {
-     return (
-         <div className="header" >
-            <img className="header-logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+import React from 'react'
+import './Header.css';
+import { SearchIcon, } from '@material-ui/icons-material/Search';
+import ShoppingCart from '../public/img/ShoppingCart';
+
+
+function Header() {
+    return (
+        <div className="header" >
+            {/* logo */}
+            <img className="header-logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+
+            {/* search bar */}
             <div className="header-search">
-                <input className="search-bar"/>
+                <input className="search-bar" />
+                <SearchIcon className="search-icon" />
             </div>
+
+            {/* nav options */}
             <div className="header-nav">
                 <div className="header-options">
-                   <div className="line1">
-                        Hello, 
+                    <div className="line1">
+                        Hello,
                     </div>
-                   <div className="line2">
-                       Sign in
+                    <div className="line2">
+                        Sign in
                     </div>
                 </div>
                 <div className="header-options">
-                <span className="line1">
+                    <span className="line1">
                         Returns
                     </span>
-                   <span className="line2">
-                       & Orders
+                    <span className="line2">
+                        & Orders
                     </span>
                 </div>
                 <div className="header-options">
-                <span className="line1">
-                        Your 
+                    <span className="line1">
+                        Your
                     </span>
-                   <span className="line2">
-                       Prime
+                    <span className="line2">
+                        Prime
                     </span>
                 </div>
                 <div className="header-cart">
-                  <img src="./cart.svg"/> 0
+                    <ShoppingCart /> 0
                 </div>
             </div>
-            
-         </div>
-     )
- }
- 
- export default Header
- 
+
+        </div>
+    )
+}
+
+export default Header
